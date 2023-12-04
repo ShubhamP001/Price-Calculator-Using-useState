187fa22bba2925e97a94adcc258150c75536fd92
Shubham <shubham101101patil@gmail.com>
2023-12-04 14:57:27 +0530

    testing address last/ required or not

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/Price-Calculator-Using-useState/"
+  base:"/Price-Calculator-Using-useState"
 })
